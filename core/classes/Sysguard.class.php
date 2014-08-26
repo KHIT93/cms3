3323f799e1b21e1781bc32e86c4a17570b0ae6d7
Kenneth <Kenneth@172.16.245.11>
2014-08-26 06:44:06 +0200

    Updated PHPMailer version in info-file from 5.2.7 to 5.2.8 Created Sysguard class for eventlog read/write/delete actions.

---
+++ core/classes/Sysguard.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file Class for handling logging of backend activity for users with access and 404, 403 and 500 errors that are not triggered by server failure.
+ */
+class Sysguard {
+    public static function get() {
+        //Gets log entries
+    }
+    public static function set() {
+        //Sets a new log entry
+    }
+    public static function clear() {
+        //Clears the log and sets a new entry after clearing the log to notify others about who cleared the log
+    }
+}
