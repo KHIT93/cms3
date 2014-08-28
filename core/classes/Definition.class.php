70a0a2a96a55b44386fafc61bf54d101d79af02c
Kenneth <Kenneth@172.16.245.11>
2014-08-28 07:25:31 +0200

    Added system definition class for handling translation, addition, updates and removal of system definitions in the database.

---
+++ core/classes/Definition.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file Class to translate, add and remove definitions from the System Definitions table
+ */
+class Definition {
+    public static function resolve($definition) {
+        //Resolves a definition and returns an object of fields with options
+    }
+}
